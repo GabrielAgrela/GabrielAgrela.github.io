7926789d8ee6c3cbd112284757676683c1ecc2ab
sopustos <100cabessa@gmail.com>
2017-04-11 18:00:20 +0100

    teste php

---
+++ POPOP/index.php
@@ -0,0 +1,238 @@
+<?php
+	include("../config.php");
+?>
+<!doctype html>
+<html lang="en-us">
+  <head>
+  
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Gabriel's portfolio</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="featherlight/bootstrap.min.css" rel="stylesheet">
+	<link href="featherlight/featherlight.min.css" type="text/css" rel="stylesheet" >
+	<link href="featherlight/featherlight.gallery.min.css" type="text/css" rel="stylesheet" >
+	<link rel="shortcut icon" href="game/TemplateData/favicon.ico" />
+    <script src="game/TemplateData/UnityProgress.js"></script>
+	<script type="text/javascript">
+		if (screen.width <= 800) {
+		window.location = "https://sopustos.github.io/";
+		}
+	</script>
+  </head>
+
+
+  <body>
+<!--------------------------------------------------------------------------------------------------Logo e Trailler -------------------------------------------------------------------------------------------->
+	<div class="parallax1">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+				
+				<img class="logo" src="images/logo.png" width="100%" height="200"/>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-sm-1">
+				</div>
+				<div class="col-sm-10">
+					<iframe class="iframecenter" width="100%" height="480" src="https://www.youtube.com/embed/9YOx9ef6Lpk?autoplay=1" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-sm-1">
+
+				</div>
+			</div>
+		</div>
+	</div>
+<!-------------------------------------------------------------------------------------------------- Galeria -------------------------------------------------------------------------------------------->
+	<div class="content">
+	  <h2>Galeria</h2>
+		<div class="container">
+		
+			<div class="row">
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>
+				
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>
+
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>
+
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>
+
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>	
+					
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>
+				
+			</div>
+			
+			<div class="row gallery2">
+			
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>
+				
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>
+
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>
+
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>
+
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>	
+					
+				<div class="col-md-2 gallery"  >
+					<a href="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="960" height="540" alt="imagem">
+						<img src="http://static3.gamespot.com/uploads/scale_super/gamespot/images/2006/317/reviews/764696-931592_20061114_001.jpg" width="170" height="120" class="pic" />
+					</a>
+				</div>
+				
+			</div>			
+			
+			
+			
+		</div>
+
+	</div>
+<!-------------------------------------------------------------------------------------------------- Jogo -------------------------------------------------------------------------------------------->
+	<div class="parallax2">
+	  <h1>MY PARALLAX</h1>
+	  <div class="container">
+		<div class="row">
+			<div class="col-sm-1">
+			</div>
+			<div class="col-sm-10">
+				<canvas class="emscripten" id="canvas" oncontextmenu="event.preventDefault()" height="600%" width="935%"></canvas>
+					<br>
+					<script type='text/javascript'>
+						var Module = {
+						TOTAL_MEMORY: 536870912,
+						errorhandler: null,			// arguments: err, url, line. This function must return 'true' if the error is handled, otherwise 'false'
+						compatibilitycheck: null,
+						dataUrl: "game/Release/pop_troca_de_arma.data",
+						codeUrl: "game/Release/pop_troca_de_arma.js",
+						memUrl: "game/Release/pop_troca_de_arma.mem",
+
+						};
+					</script>
+					<script src="game/Release/UnityLoader.js"></script>
+
+			</div>
+			<div class="col-sm-1">
+			</div>
+		</div>
+	  </div>
+	</div>
+<!-------------------------------------------------------------------------------------------------- Stats -------------------------------------------------------------------------------------------->
+	<div class="content">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+				
+				<h1>Estatisticas do jogo!</h1>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-sm-1">
+				</div>
+				<div class="col-sm-10">
+					<?php
+						if ($stmt = $mysqli->prepare("SELECT SUM(n_tiros) FROM shots;")) 
+						{
+							$stmt->execute();
+							$stmt->store_result();
+							$stmt->bind_result($shots);
+							while ($stmt->fetch()) 
+							{
+								echo "$shots";
+							}
+						}
+							else {
+							echo "ERROR: could not prepare SQL statement.";
+							echo "Debug4";
+						}
+								
+					?>
+				</div>
+				<div class="col-sm-1">
+
+				</div>
+			</div>
+		</div>
+	</div>
+<!-------------------------------------------------------------------------------------------------- BOSS FIGHT! -------------------------------------------------------------------------------------------->	
+	<div class="parallax1">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>BOSS FIGHT!</h1>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-sm-1">
+				</div>
+				<div class="col-sm-10">
+					<iframe class="iframecenter" width="100%" height="480" src="https://www.youtube.com/embed/9YOx9ef6Lpk?autoplay=1" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class="col-sm-1">
+
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="featherlight/bootstrap.min.js"></script>
+	<script src="featherlight/featherlight.min.js" type="text/javascript" charset="utf-8"></script>
+	<script src="featherlight/featherlight.gallery.min.js" type="text/javascript" charset="utf-8"></script>
+	<script>
+		$(document).ready(function(){
+			$('.gallery a').featherlightGallery();
+		});
+	</script>
+
+  </body>
+</html>
